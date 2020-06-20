b24a7f9570d34a28734ea6b77c1f93e079fcd885
orivalwu <1093077690@qq.com>
2020-06-20 11:11:37 +0800

    第二次提交

---
+++ src/main/java/com/project/webemail/WebemailApplication.java
@@ -0,0 +1,13 @@
+package com.project.webemail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebemailApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebemailApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/webemail/WebemailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.webemail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebemailApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
